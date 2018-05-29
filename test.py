7bdf4aaf4f79982955142545d952b1a9413739ff
Janet Wairimu <janetnim401@gmail.com>
2018-05-29 16:41:45 +0300

    changing the admin resolve request test

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ class User(unittest.TestCase):
 
 
 	def test_user_sign_up(self):
+		# testing user sign up entries -post
 		details = {"email":"janetnim401@gmail.com", "username":"janet", "password":"12345"}
 		response = self.checker.post('api/v1/signup', data=json.dumps(details), content_type = 'application/json')
 		self.assertEqual(response.status_code, 200)
@@ -28,6 +29,7 @@ class User(unittest.TestCase):
 		self.assertEqual(output_2, "Please use data in string format")
 
 	def test_user_log_in(self):
+		# testing user log in entries -post
 		details = {"username":"janet", "password":"12345"}
 		response = self.checker.post('api/v1/login', data=json.dumps(details), content_type = 'application/json')
 		self.assertEqual(response.status_code, 200)
@@ -41,6 +43,7 @@ class User(unittest.TestCase):
 		self.assertEqual(output_2, "Please use data in string format")
 
 	def test_user_make_request(self):
+		# testing user make a request -post
 		request_data = {"request_id": "1234", "request": "bulb repair", "department": "HR"}
 		response = self.checker.post("api/v1/request", data=json.dumps(request_data), content_type= 'application/json')
 		self.assertEqual(response.status_code, 200)
@@ -57,6 +60,7 @@ class User(unittest.TestCase):
 
 
 	def test_user_view_a_request(self):
+		# testing user viewing each request -get
 		request_data = {{"request_id":1234} : {"request":"clock repair"}}
 		response = self.checker.post('api/v1/request/', data=json.dumps(request), content_type = 'application/json')
 		output= json.loads(response.data)
@@ -73,6 +77,7 @@ class User(unittest.TestCase):
 
 
 	def test_user_view_all_requests(self):
+		# testing user view all his requests -get
 		details = {"username":"janet", "password":"12345"}
 		response = self.checker.post('api/v1/login', data=json.dumps(details), content_type = 'application/json')
 		self.assertEqual(response.status_code, 200)
@@ -84,6 +89,7 @@ class User(unittest.TestCase):
 		output = json.loads(response.data)
 
 	def test_modify_request(self):
+		# testing user modify a request, after being able to log in -update
 		request_data= {"request_id":1234, "request":"clock repair"}
 		response = self.checker.post('api/v1/request', data=json.dumps(request_data), content_type = 'application/json')
 		self.assertEqual(response.status_code, 200)
@@ -103,6 +109,7 @@ class User(unittest.TestCase):
 		self.assertEqual(response.status_code, 200)
 
 	def test_delete_a_request(self):
+		# testing a user delete a request after logging in   and making the request - delete
 		request_data = {"request_id":1234, "request":"clock repair"}
 		response = self.checker.post('api/v1/request', data=json.dumps(request_data), content_type = 'application/json')
 		self.assertEqual(response.status_code, 200)
@@ -126,6 +133,7 @@ class User(unittest.TestCase):
 		self.assertEqual(response.status_code, 200)
 
 	def test_user_gets_feedback(self):
+		# testing the user getting feedback from admin - get
 		request_data = {"request_id":1234, "request":"clock repair"}
 		response = self.checker.post('api/v1/request', data=json.dumps(request_data), content_type = 'application/json')
 		self.assertEqual(response.status_code, 200)
@@ -149,6 +157,7 @@ class User(unittest.TestCase):
 		self.assertEqual(response.status_code, 200)
 
 	def test_admin_logs_in(self):
+		# testing admin logging in - post
 		details = {"username":"elidy", "password":"454545"}
 		response = self.checker.post('api/v1/adminlogin', data=json.dumps(details), content_type = 'application/json')
 		self.assertEqual(response.status_code, 200)
@@ -162,14 +171,26 @@ class User(unittest.TestCase):
 		self.assertEqual(output_2, "Please use data in string format")
 
 	def test_admin_should_view_requests(self):
+		# testing admin viewing all requests - get
+		request_data = {"request_id":1234, "request":"clock repair", "status":"completed"}
+		response = self.checker.post('api/v1/request', data=json.dumps(request_data), content_type = 'application/json')
+		self.assertEqual(response.status_code, 200)
+
 		view_request = json.dumps(request_data)
 		response = self.checker.get("api/v1/view/1234", data=view_request, content_type= 'application/json')
 		self.assertEqual(response.status_code, 200)
 
 	def test_admin_should_resolve_requests(self):
-		request_data = {"request_id":1234, "request":"clock repair", "status":"completed"}
+		# testing admin resolve requests and update status - update
+		request_data = {"request_id":1234, "request":"clock repair", "status":"inomplete"}
 		response = self.checker.post('api/v1/request', data=json.dumps(request_data), content_type = 'application/json')
 		self.assertEqual(response.status_code, 200)
+
+		details = {"username":"elidy", "password":"454545"}
+		response = self.checker.post('api/v1/adminlogin', data=json.dumps(details), content_type = 'application/json')
+		self.assertEqual(response.status_code, 200)
+		output = json.loads(response.data)
+		self.assertEqual(output, "You have successfully logged in")
 
 		resolve_request = json.dumps({"status" : "completed"})
 		response = self.checker.put('api/resolve_requests/1234', data=modify_request, content_type = 'application/json')
