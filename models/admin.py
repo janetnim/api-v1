48b8732aea9b25a6b460936c0e625d07be1e23e5
Janet Wairimu <janetnim401@gmail.com>
2018-05-31 22:47:35 +0300

    add pycache and venv

--- models/admin.py
+++ models/admin.py
@@ -18,21 +18,29 @@ class Admin(object):
     def __init__(self):
         self.results = {}
 
-    def received_request(self):
+    def received_request(self, request_id, request, department):
         '''checks each request sent'''
         request1 = [
-            request1 for request1 in received_requests if request1[request_id] == request_id]
-        request1 = request1.split(",")
+            request1 for request1 in Admin().received_requests if request1["request_id"] == request_id]
         for request in request1:
-            return request
+            return "Request is present"
+        if len(request1) == 0:
+            return "No request present"
 
-    def view_all_requests(self, received_requests):
-        '''checks for existence of requests sent first'''
-        received_requests = received_requests.split(",")
-        if len(received_requests) == 0:
-            return "No received requests available"
-        return received_requests
+    def view_all_requests(self):
+    	#fetchs all requests sent to admin
+    	if len(Admin().received_requests) == 0:
+    		return "No received requests available"
+    	return "Requests available"
 
-    def resolve_requests(self, received_requests):
-
-		
+    def resolve_requests(self, request_id, request, department):
+    	#fetches request info for the admin to resolve
+    	if request_id == "" or request == "" or department == "":
+    		return "No request present"
+    	request2 = [
+            request1 for request1 in Admin().received_requests if request1["request_id"] == request_id]
+    	if not request2:
+    		return "No request present, you cannot resolve"
+    	return "Request is present, you can resolve"
+          			                          
+          			                          
