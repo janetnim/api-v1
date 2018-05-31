19b041cce6be4b92606d5bde3ad62a956f9532f4
Janet Wairimu <janetnim401@gmail.com>
2018-05-31 22:51:22 +0300

    add more tests to admin

--- test_admin.py
+++ test_admin.py
@@ -1,9 +1,32 @@
 import unittest
-from models.admin import Admin 
+from models.admin import Admin
 from models.user import User
+
 
 class TestModels(unittest.TestCase):
     """Test models for verion 1 api"""
-    def test_admin_receive_request(self):
-        results = Admin().received_request("", "email@email.com", 3456)
-        self.assertEqual(results, "Error! You have blank entries")
+
+    def test_admin_receive_existing_request(self):
+        results = Admin().received_request(1234, "Bulb repairs", "hr")
+        self.assertEqual(results, "Request is already present")
+
+    def test_admin_receive_new_request(self):
+        results = Admin().received_requests(1546, "Iron repair", "fianance")
+        self.assertEqual(results, "Received a new request")
+
+    def test_user_views_all_requests(self):
+    	results = Admin().view_all_requests()
+    	self.assertIsInstance(results, list, msg='Incorrect output type')
+
+    def test_admin_resolve_request_exists(self):
+        results = Admin().resolve_requests(1234)
+        self.assertEqual(results, "Request is present, you can resolve")
+
+    def test_admin_resolve_request_id_does_not_exist(self):
+        results = Admin().resolve_requests(85878)
+        self.assertEqual(
+            results, "No request present, you cannot resolve")
+
+	  
+if __name__ == '__main__':
+    unittest.main()
