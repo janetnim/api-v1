37ae93f2b187c3e39ab89f52443cd213c709c832
Janet Wairimu <janetnim401@gmail.com>
2018-05-31 22:52:43 +0300

    add admin api endpoints

--- test_api.py
+++ test_api.py
@@ -35,37 +35,53 @@ class User(unittest.TestCase):
         # testing user viewing each request -get
         request_data = {1234}
         response = self.checker.post(
-            '/api/v1/view/1234', data=json.dumps(request_data), content_type='application/json')
+            '/api/v1/view/1234', data=json.dumps(User().request_data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     def test_user_view_all_requests(self):
         # testing user view all his requests -get
         response = self.checker.post(
-            '/api/v1/view', data=json.dumps(request_data), content_type='application/json')
+            '/api/v1/view', data=json.dumps(User().request_data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     def test_modify_request(self):
-        # testing user modify a request, after being able to log in -update
-        request_data = {1234}
+        # testing user modify a request -update
+        request_data = {5555}
         response = self.checker.post(
-            '/api/v1/modify/1234', data=json.dumps(request_data), content_type='application/json')
+            '/api/v1/modify/1234', data=json.dumps(User().request_data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     def test_delete_a_request(self):
-        # testing a user delete a request after logging in   and making the request - delete
-        request_data = {1234}
+        # testing a user delete a request -delete
+        request_data = {5478}
         response = self.checker.post(
-            '/api/v1/delete/1234', data=json.dumps(request_data), content_type='application/json')
+            '/api/v1/delete/1234', data=json.dumps(User().request_data), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
 
-class Admin(object):
-	def test_admin_receive_request(self):
-		
+class Admin(unittest.TestCase):
+    '''def test_receive_request(self):
+        # testing an admin receive requests from users -get
+        received_requests = {7777, "curtains repair", "media"}
+	    response = self.checker.post('/api/v1/adminrequest/7777', data=json.dumps(
+	        received_requests), content_type='application/json')
+	    self.assertEqual(response.status_code, 201)'''
+    def setUp(self):
+	    self.checker = app.test_client()
 
-	def test_admin_view_requests(self):
+    def test_view_request(self):
+        # testing an admin view all requests sent -get
+	    response = self.checker.post(
+	        '/api/v1/adminview', data=json.dumps(Admin().received_requests), content_type='application/json')
+	    self.assertEqual(response.status_code, 201)
+'''
+    def test_resolve_request(self):
+        # testing an admin resolve each relevant request
+        received_requests = {1234}
+	    response = self.checker.post(
+	        '/api/v1/resolve/1234', data=json.dumps(received_requests), content_type='application/json')
+	    self.assertEqual(response.status_code, 201)'''
 
-	def test_admin_reslove_requests(self):
 
 if __name__ == '__main__':
     unittest.main()
