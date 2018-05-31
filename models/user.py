18419bcb64954cf03803618a4b287ca46f56910d
Janet Wairimu <janetnim401@gmail.com>
2018-05-31 22:49:54 +0300

    change logic of functions

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@ from .admin import  Admin
 class User(object):
 
     user = [{'username': 'janet', 'email': 'janetnim401@gmail.com', 'password': 66555}, {'username': 'kate', 'email': 'kate@gmail.com','password': 999454}, {'username': 'admin', 'email': 'admin@gmail.com', "password": 121212}]
-    request_data = request_data = [{
+    request_data = [{
         'request_id': 1234,
         'request': 'Bulb repairs',
         'department': 'hr'
@@ -38,7 +38,7 @@ class User(object):
         self.result[email] = email
         self.result[password] = password
         User.user.append(self.result)
-        return "You have succcessfully been registered!"
+        return "You have successfully been registered!"
 
     def login(self, username, password):
         ''' validation that the user already exists'''
@@ -50,7 +50,7 @@ class User(object):
 
         user1 = [user1 for user1 in User().user if user1["username"] ==
                                                          username and user1["password"] == password]
-        if user1 not in User().user["password"]:
+        if not user1:
             return "Your account does not exist, please sign sign_up"    
         return "You have successfully logged_in"
 
@@ -63,49 +63,45 @@ class User(object):
         '''checks if an empty request has been  entered or if it already exists'''
         if request_id == "" or request == "" or department == "":
             return "Error! Please fill all the details"
-        request1 == [request1 for request1 in User(
+        request2 = [request1 for request1 in User(
         ).request_data if request1["request_id"] == request_id]
-        if request1 in request_data:
+        if request2:
             return "Invalid request id"
         
-        self.result[request_id] = request_id
-        self.result[request] = request
-        self.result[department] = department
-        Admin.received_requests.append(self.result)
-        User.request_data.append(self.result)
+        self.result["request_id"] = request_id
+        self.result["request"] = request
+        self.result["department"] = department
+        Admin().received_requests.append(self.result)
+        User().request_data.append(self.result)
         return "Successfully made a request"
 
     def view_a_request(self, request_id):
         '''fetches a specific user request using the request's id'''
         request1 = [request1 for request1 in User(
         ).request_data if request1["request_id"] == request_id]
-        if request1 not in request_data:
+        if not request1:
             return "Request not present"
         return "Request is present"
 
     def view_all_requests(self):
-        '''fetches all user requests if present'''
-        request_data = request_data.split(",")
-        if len(request_data) == 0:
-            return "No requests available"
-        return "Requests are available"
+        return User().request_data
 
-    def modify_request(self, request_id):
+    def modify_request(self, request_id, new_request):
         '''checks for existence of a request by its id before modifying it'''
         request1 = [request1 for request1 in User(
         ).request_data if request1["request_id"] == request_id]
-        if request1 not in request_data:
+        if not request1: 
             return "Error! request does not exist"
-        else:
-            request = request_data["request"]
-            result['request'] = 'vehicle maintenance'
+
+        mod_request = request1[0]
+        mod_request['request'] = new_request
         return 'Changes successfully made'
 
     def delete_request(self, request_id):
         '''checks for existence of a request by its id before deleting it'''
         request1 = [request1 for request1 in User(
         ).request_data if request1["request_id"] == request_id]
-        if request1 not in request_data:
+        if not request1:
             return "Error! The request does not exist"
         User().request_data.remove(request1[0])
         Admin().received_requests.remove(request1[0])
