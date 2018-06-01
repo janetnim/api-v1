fe5918f84064b679d9b5a59970464a197711b047
Janet Wairimu <janetnim401@gmail.com>
2018-06-01 11:01:49 +0300

    edit admin api endpoints test

--- test_api.py
+++ test_api.py
@@ -5,7 +5,7 @@ from models.admin import Admin
 from models.user import User
 
 
-class User(unittest.TestCase):
+class TestApis(unittest.TestCase):
     def setUp(self):
         self.checker = app.test_client()
 
@@ -26,61 +26,51 @@ class User(unittest.TestCase):
 
     def test_user_make_request(self):
         # testing user make a request -post
-        request_data = {1255, "machine fixing", "finance"}
+        request_data = {"request_id": 1255,
+            "request": "machine fixing", "department": "finance"}
         response = self.checker.post(
-            '/api/v1/make_request', data=json.dumps(request_data), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+            '/api/v1/makerequest', data=json.dumps(request_data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
     def test_user_view_a_request(self):
         # testing user viewing each request -get
-        request_data = {1234}
-        response = self.checker.post(
-            '/api/v1/view/1234', data=json.dumps(User().request_data), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        request_data = {"request_id": 1234}
+        response = self.checker.get(
+            '/api/v1/view/1234', data=json.dumps(request_data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
     def test_user_view_all_requests(self):
         # testing user view all his requests -get
-        response = self.checker.post(
-            '/api/v1/view', data=json.dumps(User().request_data), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        response = self.checker.get(
+            '/api/v1/view', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
     def test_modify_request(self):
-        # testing user modify a request -update
-        request_data = {5555}
-        response = self.checker.post(
-            '/api/v1/modify/1234', data=json.dumps(User().request_data), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        '''testing user modify a request -update'''
+        request_data = {"request_id" :5555, "new request": "vehicle maintenance"}
+        response = self.checker.put(
+            '/api/v1/modify/1234', data=json.dumps(request_data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
     def test_delete_a_request(self):
-        # testing a user delete a request -delete
-        request_data = {5478}
-        response = self.checker.post(
-            '/api/v1/delete/1234', data=json.dumps(User().request_data), content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        '''testing a user delete a request -delete'''
+        request_data = {"request_id": 5478}
+        response = self.checker.delete(
+            '/api/v1/delete/1234', data=json.dumps(request_data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_receive_request(self):
+    	response = self.checker.get(
+	        '/api/v1/adminrequest', content_type='application/json')
+    	self.assertEqual(response.status_code, 200)  
 
 
-class Admin(unittest.TestCase):
-    '''def test_receive_request(self):
-        # testing an admin receive requests from users -get
-        received_requests = {7777, "curtains repair", "media"}
-	    response = self.checker.post('/api/v1/adminrequest/7777', data=json.dumps(
-	        received_requests), content_type='application/json')
-	    self.assertEqual(response.status_code, 201)'''
-    def setUp(self):
-	    self.checker = app.test_client()
-
-    def test_view_request(self):
-        # testing an admin view all requests sent -get
-	    response = self.checker.post(
-	        '/api/v1/adminview', data=json.dumps(Admin().received_requests), content_type='application/json')
-	    self.assertEqual(response.status_code, 201)
-'''
-    def test_resolve_request(self):
-        # testing an admin resolve each relevant request
-        received_requests = {1234}
-	    response = self.checker.post(
-	        '/api/v1/resolve/1234', data=json.dumps(received_requests), content_type='application/json')
-	    self.assertEqual(response.status_code, 201)'''
+    # def test_resolve_request(self):
+    #     '''testing an admin resolve each relevant request'''
+    #     received_requests = {"request_id":1234}
+    #     response = self.checker.get(
+	   #      '/api/v1/resolve/1234', data=json.dumps(received_requests), content_type='application/json')
+    #     self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
