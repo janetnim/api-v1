fb883604a3e20c9ea6e3d2926f2aabf6cf292da1
Janet Wairimu <janetnim401@gmail.com>
2018-06-02 03:42:00 +0300

    [Chore#157990650]delete resolve request function

--- models/admin.py
+++ models/admin.py
@@ -13,14 +13,3 @@ class Admin(object):
         res = Admin().received_requests
         return res
 
-    # def resolve_requests(self, request_id):
-    # 	'''fetches request info for the admin to resolve'''
-    # 	Admin().received_requests.append(User().request_data)
-    # 	request2 = [
-    #         request1 for request1 in Admin().received_requests if request1["request_id"] == request_id]
-    # 	if request2:
-    # 		return "Request is present, you can resolve"
-    # 	return "No request present, you cannot resolve"
-    	
-          			                          
-          			                          
