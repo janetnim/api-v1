4542b24f85cda6335f892156d3750d8136762333
Janet Wairimu <janetnim401@gmail.com>
2018-06-06 16:30:23 +0300

    add databse configuration

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+from configparser import ConfigParser
+
+def config(filename="database.init", section="postgresql"):
+	parser = ConfigParser()
+	parser.read(filename)
+
+	db={}
+	if parser.has_section(section):
+		params = parser.itemms(section)
+		for param in params:
+			db[param[0]] = param[1]
+	else:
+		raise Exception('section {0} not found in the {1} jfile'.format(section, filename))
+
+	return db
