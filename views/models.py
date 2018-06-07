efb03e37d10acbd7d76e825724019b4f7a0a5437
Janet Wairimu <janetnim401@gmail.com>
2018-06-07 10:36:35 +0300

    add admin requests endpoint

--- views/models.py
+++ views/models.py
@@ -1,43 +1,71 @@
-class Authentication(Resource):
-	cur.execute("CREATE TABLE IF NOT EXISTS users (personal_id serial PRIMARY KEY, username varchar unique, email varchar unique, password varchar unique)")
-	def post_signup(self, username="", email="", password=""):
-		if username="" or email="" or password="":
+from flask_restful import Resource, reqparse
+from  flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity, get_raw_jwt)
+from flask import jsonify
+
+
+class User_SignUp(Resource):
+	def post_signup(self, username, email, password):
+		if username=="" or email=="" or password=="":
 			return "Please enter all details"
 		cur.execute("SELECT username FROM users")
 		res = cur.fetchall()
 		if username in res:
 			return "The user already exists"
+		password = generate_password_hash((password), method='sha256')
 		cur.execute("INSERT INTO users (username, email, password)VALUES('{}','{}','{}')".format(username, email, password))
 		cur.execute("SELECT * FROM users WHERE username={}".format(username))
 		conn.commit()
-			return "User successfully signed up"
+		return "User successfully signed up"
 
-# CHANGE OUTPUT TO JSONIFY
+class User_login(Resource):
+	def get_one_user(self):
+		cur.execute("SELECT * FROM usrs WHERE username =%s", (username,))
+		user = cur.fetchone()
+		conn.commit()
+		return{
+		"personal_id":user[0],
+		"username":user[1],
+		"email":user[2],
+		"password":user[3]
+		}
 
-	def post_login(self, username="", password=""):
-		if username="" or password="":
-			return "Enter all details"
+	def post(self):
+		username = request.json.get("username")
+		password = request.json.get("password")
+
+		parser = reqparse.RequestParser()
+		parser.add_argument('username', type=str, help='invalid username')
+		args = parser.parse_args()
+
+		user = get_user(username)
+		if user is None:
+			return jsonify({"message":"user not found"}), 404
+		elif user['password'] != password:
+			return jsonify({"message":"incorrect password"})
+		else:
+			token = create_access_token(identity=request.json.get("username"))
+			return jsonify({"message": "logged in successfully", "token":token})
+		return make_response("cannot verify", 401, {"WWW-Authentication": "Basic realm='Login required'"})
+
+		if username=="" or password=="":
+			return jsonify({"message":"Enter all details"})
 		cur.execute("SELECT username FROM users")
 		res = cur.fetchall()
 		if username not in res:
-			return "You are not a user"
+			return jsonify({"message":"You are not a user"})
 		conn.commit()
-		return "you have logged in successfully"
-		# NOTE TO SELF: the above use api auth/signup or auth/login
+		return jsonify({"message":"you have logged in successfully"})
 
 
-class User(Resource):
-	cur.execute("CREATE TABLE IF NOT EXISTS requests (request_id serial PRIMARY_KEY, request varchar, department varchar, personal_id integer REFERENCES users (personal_id))")
-	cur.execute("SELECT * FROM users WHERE username != 'admin'")
-
-	def post_make_request(self, request={}, department={}):
-		if request="" or department="":
+class Request(Resource):
+	def post_make_request(self, request={}, department={}, status="Sent"):
+		if request=="" or department=="":
 			return "Please fill all details"
 		cur.execute("INSERT INTO user(request, department)VALUES('{}', '{}')".formart(request, department))
 		cur.execute("SELECT * FROM requests")
 		res = cur.fetchone()
 		conn.commit()
- 		return res
+		return res
 
 
 	def get_view_a_request(self, request_id):
@@ -48,17 +76,16 @@ class User(Resource):
 		return res
 	
 
-	def get_view_all_requests(self):
-		cur.execute("SELECT * FROM requests")
+	def get_view_all_requests(self, personal_id):
+		cur.execute("SELECT * FROM requests WHERE personal_id={}".format(personal_id))
 		res =cur.fetchall()
 		if res is None:
 			return "No requests available"
 		return res
-		# NOTE TO SELF: the view all requests gets api/v2/users/requests
 	
 
-	def put_modify_request(self, request_id):
-		cur.execute("SELECT request_id FROM requests WHERE request_id={}".format(request_id))
+	def put_modify_request(self, request_id, personal_id):
+		cur.execute("SELECT request_id FROM requests WHERE request_id={}, personal_id={}".format(request_id, personal_id))
 		res = cur.fetchone()
 		if res is None:
 			return "Request not present"
@@ -69,8 +96,8 @@ class User(Resource):
 		return items
 
 	
-	def delete_a_request(self, request_id):
-		cur.execute("SELECT * FROM requests WHERE request_id={}".format(request_id))
+	def delete_a_request(self, request_id, personal_id):
+		cur.execute("SELECT * FROM requests WHERE request_id={}".format(request_id, personal_id))
 		res = cur.fetchone()
 		if res is None:
 			return "Request does not exist!"
@@ -78,36 +105,45 @@ class User(Resource):
 		cur.execute("SELECT * FROM requests")
 		conn.commit()
 		return "Deleted successfully"
-	#NOTE TO SELF: the above use api/v2/users/requests/.... or api/v2/users/requests
+
+	def get_all_requests_admin(self):
+		cur.execute("SELECT * FROM requests")
+		res =cur.fetchall()
+		if res is None:
+			return "No requests available"
+		return res
+		
+	def put_approve_request(self, request_id):
+		cur.execute("SELECT request_id FROM requests WHERE request_id={}".format(request_id))
+		res=cur.fetchone()
+		if res is None:
+			return "Request not present"
+		# change status to approve
+		# then commit
+
+	def put_disapprove_request(self, request_id):
+		cur.execute("SELECT request_id FROM requests WHERE request_id={}".format(request_id))
+		res=cur.fetchone()
+		if res is None:
+			return "Request not present"
+		# change status to disapprove
+		# then commit
+
+	def put_resolve_request(self, request_id):
+		cur.execute("SELECT request_id FROM requests WHERE request_id={}".format(request_id))
+		res=cur.fetchone()
+		if res is None:
+			return "Request not present"
+		# change status to complete
+		# then commit
+
+	def delete_request(self, request_id):
+		pass
 
 
-# class Admin(Resource):
-# 	cur.execute("CREATE TABLE IF NOT EXISTS admin (request_id serial PRIMARY_KEY, request varchar, department varchar)")
-# 	cur.execute("SELECT * FROM admin WHERE username = 'admin'")
-# 	admin = cur.fetchone()
-# 	if admin is None:
-# 		# cur.execute("INSERT INTO(request, department)VALUES('{}','{}')".format(request,department))
-
-# 	def get_all_requests:
-# 		res = cur.execute("SELECT * FROM admin")
-# 		if res is None:
-# 			return "No requests available"
-# 		items = cur.fetchall()
-# 		return items
-
-# 	def put_approve_request:
-# 		pass
-# 	def put_disapprove_request:
-# 		pass
-# 	def put_resolve_request:
-# 		pass 
-# 	NOTE TO SELF: the above uses the api: api/v2/requests/<reqid>/<resolve/disapprove/approve>
-# 	NOTE TO SELF: while view all requests is api/v2/requests
-
-api.add_resource(Authenication, 'api/v2/auth/')
-api.add_resource(User, 'api/v2/users/requests')
-api.add_resource(User, 'api/v2/users/requests/<int:request_id>')
-api.add_resource(Admin, 'api/v2/requests')
-api.add_resource(Admin, 'api/v2/requests/<int:request_id>')
-# cur.close()
-# con.close()
+		# cur.execute("SELECT request_id FROM requests WHERE request_id={}".format(request_id))
+		# res=cur.fetchone()
+		# if res is None:
+		# 	return "Request not present"
+		# change status to approve
+		# if status is complete, delete then commit
