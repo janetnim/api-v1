22787adb4b68f840df6ce1e0dce117d0458436cd
Janet Wairimu <janetnim401@gmail.com>
2018-06-07 20:16:25 +0300

    [Chore]create users and requests tables

---
+++ manage.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+
+conn = psycopg2.connect("dbname='maintenance' user='postgres' host='localhost' password='123456'")
+cur = conn.cursor()
+
+
+def create_tables():
+	try:
+
+		cur.execute("CREATE TABLE IF NOT EXISTS users (personal_id serial PRIMARY KEY, username varchar unique, email varchar unique, password varchar unique)")
+		cur.execute("CREATE TABLE IF NOT EXISTS requests (request_id serial PRIMARY KEY, request varchar, department varchar, status varchar,personal_id integer REFERENCES users (personal_id))")
+		cur.execute("SELECT * FROM users WHERE username != 'admin'")
+
+		conn.commit()
+
+	except:
+		print("Cannot connect to database")
+
+
+
+create_tables()
