1873c3cf6cb053f84fb50153f2d8af2866b7f64a
Janet Wairimu <janetnim401@gmail.com>
2018-06-08 12:43:41 +0300

    [chore] create app

--- run.py
+++ run.py
@@ -6,28 +6,32 @@ import manage
 from models import User_login, User_SignUp, MakeRequest, ViewAllRequest, RequestView, ModifyRequest, DeleteRequest, AdminGetRequest, ApproveRequest, DisapproveRequest,AdminResolveRequest, AdminDeleteRequest
 # from test.test_api import TestModels
 
-
-app = Flask(__name__)
-api = Api(app)
-
-app.config['JWT_SECRET_KEY'] = "my awesome secret key"
-
-jwt = JWTManager(app)
+def create_app():
 
 
-api.add_resource(User_SignUp, '/auth/signup', methods=['POST'])
-api.add_resource(User_login, '/auth/login', methods=['POST'])
-api.add_resource(MakeRequest, '/users/requests', methods=['POST'])
-api.add_resource(ViewAllRequest, '/users/requests', methods=['GET'])
-api.add_resource(RequestView, '/users/requests/<int:request_id>', methods=['GET'])
-api.add_resource(ModifyRequest, '/users/requests/<int:request_id>', methods=['PUT'])
-api.add_resource(DeleteRequest, '/users/requests/<int:request_id>', methods=['DELETE'])
-api.add_resource(AdminGetRequest, '/requests', methods=['GET'])
-api.add_resource(ApproveRequest, '/requests/<int:request_id>/approve', methods=['PUT'])
-api.add_resource(DisapproveRequest, '/requests/<int:request_id>/disapprove', methods=['PUT'])
-api.add_resource(AdminResolveRequest, '/requests/<int:request_id>/resolve', methods=['PUT'])
-api.add_resource(AdminDeleteRequest, '/requests/<int:request_id>/delete', methods=['DELETE'])
+	app = Flask(__name__)
+	api = Api(app)
+
+	app.config['JWT_SECRET_KEY'] = "my awesome secret key"
+
+	jwt = JWTManager(app)
+
+
+	api.add_resource(User_SignUp, '/auth/signup', methods=['POST'])
+	api.add_resource(User_login, '/auth/login', methods=['POST'])
+	api.add_resource(MakeRequest, '/users/requests', methods=['POST'])
+	api.add_resource(ViewAllRequest, '/users/requests', methods=['GET'])
+	api.add_resource(RequestView, '/users/requests/<int:request_id>', methods=['GET'])
+	api.add_resource(ModifyRequest, '/users/requests/<int:request_id>', methods=['PUT'])
+	api.add_resource(DeleteRequest, '/users/requests/<int:request_id>', methods=['DELETE'])
+	api.add_resource(AdminGetRequest, '/requests', methods=['GET'])
+	api.add_resource(ApproveRequest, '/requests/<int:request_id>/approve', methods=['PUT'])
+	api.add_resource(DisapproveRequest, '/requests/<int:request_id>/disapprove', methods=['PUT'])
+	api.add_resource(AdminResolveRequest, '/requests/<int:request_id>/resolve', methods=['PUT'])
+	api.add_resource(AdminDeleteRequest, '/requests/<int:request_id>/delete', methods=['DELETE'])
+	return app
 
 
 if __name__ == '__main__':
+	app = create_app()
 	app.run(debug=True)
