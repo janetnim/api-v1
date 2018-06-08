ca18ae3cf63255ca9389b89ab633d1901a3756aa
Janet Wairimu <janetnim401@gmail.com>
2018-06-08 09:20:14 +0300

    [chore] add personal id column for requests

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ cur = conn.cursor()
 def create_tables():
 	try:
 
-		cur.execute("CREATE TABLE IF NOT EXISTS users (personal_id serial PRIMARY KEY, username varchar unique, email varchar unique, password varchar unique)")
+		cur.execute("CREATE TABLE IF NOT EXISTS users (personal_id serial PRIMARY KEY, username varchar unique NOT NULL, email varchar unique NOT NULL, password varchar unique NOT NULL)")
 		cur.execute("CREATE TABLE IF NOT EXISTS requests (request_id serial PRIMARY KEY, request varchar, department varchar, status varchar, personal_id integer REFERENCES users (personal_id))")
 		cur.execute("SELECT * FROM users WHERE username != 'admin'")
 
