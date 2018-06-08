10f0ec63318dcac7637be11a6bd557bf95aebb50
Janet Wairimu <janetnim401@gmail.com>
2018-06-08 09:21:21 +0300

    [feature] update modify request method by user

--- models.py
+++ models.py
@@ -41,7 +41,6 @@ class User_login(Resource):
 		}
 
 	def post(self):
-
 		parser = reqparse.RequestParser()
 		parser.add_argument('username', type=str, help='invalid username')
 		parser.add_argument('password', type=str, help='invalid password')
@@ -79,41 +78,45 @@ class MakeRequest(Resource):
 		parser.add_argument('request', type=str, help='invalid request')
 		parser.add_argument('department', type=str, help='invalid department')
 		parser.add_argument('status', type=str, help='invalid status')
+		parser.add_argument('personal_id', type=str, help='invalid id')
 		args = parser.parse_args()
 
 		request = args['request']
 		department = args['department']
 		status = args['status']
+		personal_id = args['personal_id']
 
-		if request=="" or department=="":
+		if request=="" or department=="" or status=="" or personal_id == "":
 			return jsonify({"message":"Please fill all details"})
-		cur.execute("INSERT INTO requests (request, department, status)VALUES('{}','{}','Pending')".format(request, department))
+		cur.execute("INSERT INTO requests (request, department, status, personal_id)VALUES('{}','{}','Pending', {})".format(request, department, personal_id))
 		cur.execute("SELECT * FROM requests")
-		res = cur.fetchone()
+		res = cur.fetchall()
 		conn.commit()
 		return jsonify({"res":res})
 
+
 class RequestView(Resource):
-	def get(self, request_id):
-		cur.execute("SELECT * FROM requests WHERE request_id={}".format(request_id))
+	def get(self, request_id, personal_id):
+		cur.execute("SELECT * FROM requests WHERE request_id={} and personal_id={}".format(request_id, personal_id))
 		res = cur.fetchone()
 		if res is None or len(res) == 0:
 			return jsonify({"message":"Request not found"})
 		return jsonify({"res":res})
 
+
 class ModifyRequest(Resource):
-	def put(self, request_id):
+	def put(self, request_id, personal_id):
 		parser = reqparse.RequestParser()
 		parser.add_argument('request', type=str, help='invalid request')
 		args = parser.parse_args()
 
-		new_request = args['request']
+		request = args['request']
 
-		cur.execute("SELECT * FROM requests WHERE request_id={}".format(request_id))
+		cur.execute("SELECT * FROM requests WHERE request_id={} and personal_id={}".format(request_id, personal_id))
 		res = cur.fetchone()
 		if res is None or len(res) == 0:
-			return jsonify({"message":"Request does not exist"})
-		cur.execute("UPDATE requests SET request={} WHERE request_id={}".format(new_request, request_id));
+			return jsonify({"message": "Request does not exist"})
+		cur.execute("UPDATE requests SET request={} WHERE request_id={} and personal_id={}".format(request, request_id, personal_id));
 		cur.execute("SELECT * FROM requests WHERE request_id={}".format(request_id))
 		conn.commit()
 		items= cur.fetchone()
@@ -121,8 +124,8 @@ class ModifyRequest(Resource):
 
 
 class DeleteRequest(Resource):
-	def delete(self, request_id):
-		cur.execute("SELECT * FROM requests WHERE request_id={}".format(request_id))
+	def delete(self, request_id, personal_id):
+		cur.execute("SELECT * FROM requests WHERE request_id={} and personal_id={}".format(request_id, personal_id))
 		res = cur.fetchone()
 		if res is None or len(res) == 0:
 			return "Request does not exist!"
@@ -175,7 +178,6 @@ class DisapproveRequest(Resource):
 		conn.commit()
 		items= cur.fetchone()
 		return jsonify({"items":items})
-
 
 
 class AdminResolveRequest(Resource):
