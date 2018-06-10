dd329e422d7ecf2753ed651b243196b580ec2d1c
Janet Wairimu <janetnim401@gmail.com>
2018-06-10 13:46:05 +0300

    import re

--- models.py
+++ models.py
@@ -1,14 +1,12 @@
 from flask_restful import Resource, reqparse
 from  flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity, get_raw_jwt)
 from flask import jsonify
-from manage import create_tables,cur,conn
 import psycopg2
-import helper
+from run import helper
 import re
 # from werkzeug.security import generate_password_hash, \
 #      check_password_hash
 
-create_tables()
 
 class User_SignUp(Resource):
 	def post(self):
@@ -28,12 +26,14 @@ class User_SignUp(Resource):
 		elif username == " " or email == " " or password == " ":
 			return "Invalid entry try again"
 		elif re.match(r'^.+@([?)[a-zA-Z0-9-.])+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$' , email) is None:
-			return jsonify({"message": "invalid email"})
-		elif not isinstance(username, str)or not isinstance(email, str) or not isinstance(password, str):
-			return jsonify({"message": "Enter a string value for username, email and password"})
-		res = helper.get_users_by_username()
-		if res is not None and username in res:
-			return "The user already exists"
+			return {"message": "invalid email"}, 400
+		elif re.match(r'^[0-9]+$', username) is not None:
+			return {"message": "invalid username"}, 400
+		elif not isinstance(username, str) or not isinstance(password, str):
+			return {"message": "Enter a string value for username and password"}
+		res = helper.get_user_by_username(username)
+		if res is not None:
+			return {"message": "The user already exists"}, 400
 		helper.add_user(username,password, email)
 		helper.get_user_by_username(username)
 		return "User successfully signed up"
@@ -45,6 +45,8 @@ class User_login(Resource):
 		return user
 
 	def post(self):
+		print("==============================================")
+		print(helper.conn)
 		parser = reqparse.RequestParser()
 		parser.add_argument('username', type=str, help='invalid username')
 		parser.add_argument('password', type=str, help='invalid password')
@@ -56,26 +58,27 @@ class User_login(Resource):
 		helper.get_user_by_username_and_password(username, password)
 		user = User_login().get_one_user(username)
 		if user is None or len(user)==0:
-			return jsonify({"message":"user not found"}), 404
+			return {"message":"user not found"}, 404
 		elif user['username'] != username:
-			return jsonify({"message":"incorrect username"})
+			return {"message":"incorrect username"}
 		elif user['password'] != password:
-			return jsonify({"message":"incorrect password"})
+			return {"message":"incorrect password"}
+		elif re.match(r'^[0-9]+$', username) is not None:
+			return {"message": "invalid username"}, 400
 		else:
 			token = create_access_token(identity=username)
-			return jsonify({"message": "logged in successfully", "token":token})
+			return {"message": "logged in successfully", "token":token}
 		return make_response("cannot verify", 401, {"WWW-Authentication": "Basic realm='Login required'"})
 		if not isinstance(username, str) or not isinstance(password, str):
-			return jsonify({"message": "Enter a string value for username and password"})
+			return {"message": "Enter a string value for username and password"}
 		if username=="" or password=="":
-			return jsonify({"message":"Enter all details"})
+			return {"message":"Enter all details"}
 		if len(username.split()) == 0 or len(password.split())==0:
-			return jsonify({"message": "Invalid entry try again"})
+			return {"message": "Invalid entry try again"}
 		helper.get_users_by_username()
-		
 		if username not in res:
-			return jsonify({"message":"You are not a user"})
-		return jsonify({"message":"you have logged in successfully"})
+			return {"message":"You are not a user"}
+		return {"message":"you have logged in successfully"}
 
 
 class MakeRequest(Resource):
@@ -92,22 +95,22 @@ class MakeRequest(Resource):
 		status = args['status']
 
 		if request =="" or department=="":
-			return jsonify({"message": "Please fill all details"})
+			return {"message": "Please fill all details"}
 		elif len(request.split()) == 0 or len(department.split())==0:
-			return jsonify({"message": "Invalid entry try again"})
+			return {"message": "Invalid entry try again"}
 		elif not isinstance(request, str) or not isinstance(department, str):
-			return jsonify({"message": "Enter a string value for request and department"})
+			return {"message": "Enter a string value for request and department"}
 		helper.insert_user_request(request, department)
-		return jsonify({"message":"request made successfully"})
+		return {"message":"request made successfully"}
 
 
 class RequestView(Resource):
 	@jwt_required
 	def get(self, request_id):
-		res = helper.get_user_request(request_id)
+		res = helper.get_user_request(get_jwt_identity())
 		if res is None or len(res) == 0:
-			return jsonify({"message":"Request not found"})
-		return jsonify({"res":res})
+			return {"message":"Request not found"}
+		return {"res":res}
 
 
 class ModifyRequest(Resource):
@@ -123,21 +126,21 @@ class ModifyRequest(Resource):
 
 		res = helper.get_user_request(request_id)
 		if res is None or len(res) == 0:
-			return jsonify({"message": "Request does not exist"})
+			return {"message": "Request does not exist"}
 		helper.modify_user_request(request_id)
 		helper.get_request_by_id(request_id)
 		items= cur.fetchone()
-		return jsonify({"items":items})
+		return {"items":items}
 
 
 class DeleteRequest(Resource):
 	def delete(self, request_id):
 		res = helper.get_user_request(request_id)
 		if res is None or len(res) == 0:
-			return jsonify({"message":"Request does not exist!"})
+			return {"message":"Request does not exist!"}
 		helper.delete_request_by_id(request_id)
 		helper.admin_get_all_requests()
-		return jsonify({"message":"Deleted successfully"})
+		return {"message":"Deleted successfully"}
 
 
 class ViewAllRequest(Resource):
@@ -145,7 +148,7 @@ class ViewAllRequest(Resource):
 		res = helper.user_get_all()
 		if res is None or len(res) == 0:
 			return "No requests available"
-		return jsonify({"res":res})
+		return {"res":res}
 
 	
 class AdminGetRequest(Resource):
@@ -154,7 +157,7 @@ class AdminGetRequest(Resource):
 		helper.admin_get_all_requests()
 		if res is None or len(res) == 0:
 			return "No requests available"
-		return jsonify({"res":res})
+		return {"res":res}
 
 
 class ApproveRequest(Resource):
@@ -162,11 +165,11 @@ class ApproveRequest(Resource):
 	def put(self, request_id):
 		res = get_request_by_id(request_id)
 		if res is None:
-			return jsonify({"message":"Request does not exist"})
+			return {"message":"Request does not exist"}
 		helper.approve_request_by_id(request_id)
 		helper.get_request_by_id(request_id)
 		items= cur.fetchone()
-		return jsonify({"items":items})
+		return {"items":items}
 
 
 class DisapproveRequest(Resource):
@@ -174,11 +177,11 @@ class DisapproveRequest(Resource):
 	def put(self, request_id):
 		get_request_id(request_id)
 		if res is None:
-			return jsonify({"message":"Request does not exist"})
+			return {"message":"Request does not exist"}
 		helper.disapprove_request_by_id(request_id)
 		helper.get_request_by_id(request_id)
 		items= cur.fetchone()
-		return jsonify({"items":items})
+		return {"items":items}
 
 
 class AdminResolveRequest(Resource):
@@ -186,11 +189,11 @@ class AdminResolveRequest(Resource):
 	def put(self, request_id):
 		res = helper.get_request_by_id(request_id)
 		if res is None or len(res)==0:
-			return jsonify({"message":"Request does not exist"})
+			return {"message":"Request does not exist"}
 		helper.resolve_request_by_id(request_id)
 		helper.get_request_by_id(request_id)
 		items= cur.fetchone()
-		return jsonify({"items":items})
+		return {"items":items}
 
 
 class AdminDeleteRequest(Resource):
@@ -202,7 +205,7 @@ class AdminDeleteRequest(Resource):
 			return "Request does not exist!"
 		helper.delete_request_by_id(request_id)
 		helper.admin_get_all_requests()
-		return jsonify({"message":"Deleted successfully"})
+		return {"message":"Deleted successfully"}
 
 if __name__ == '__main__':
 	print(User_login().get_one_user("kiki"))
