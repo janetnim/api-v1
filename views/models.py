d988a952796f28af8b14ef2291863cfafa12d7d2
Janet Nim <janetnim401@gmail.com>
2018-06-23 21:05:29 +0300

    get role of user

--- views/models.py
+++ views/models.py
@@ -83,9 +83,10 @@ class User_login(Resource):
 			return {"message":"Enter all details"}
 		if len(username.split()) == 0 or len(password.split())==0:
 			return {"message": "Invalid entry try again"}
+		#role = get_role(username)
 
 		token = create_access_token(identity=username)
-		return {"message": "You have logged in successfully", "token":token, "role":role}
+		return {"message": "You have logged in successfully", "token":token}
 
 
 class MakeRequest(Resource):
